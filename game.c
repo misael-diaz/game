50db0e2755541f8c22b9e115b9c5abac46dea0b6
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-05-17 13:27:09 -0400

    disables terminal input echoing
    NOTES
    if we don't we are going to see whatever the user inputs on the
    terminal and that's not what you want when playing a game
    
    the most useful resource was the GLIBC documentation and the
    man pages, these resources have been added to the reference section
    at the end of the source
    
    we also do not want to output to the user when the key is either
    pressed or released but not both so we have added code to handle
    that; bear in mind that we do not yet know which one is being
    handled (pressed or released), gotta look at more documentation.

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 #include <linux/input.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +11,16 @@
 
 int main ()
 {
+	// disables terminal input echoing
+	struct termios term = {};
+	struct termios *terp = &term;
+	tcgetattr(STDIN_FILENO, terp);
+	terp->c_lflag &= (~ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, terp);
+	terp->c_lflag |= ECHO;
+
 	errno = 0;
-	struct input_event ev;
+	struct input_event ev = {};
 	struct input_event *evp = &ev;
 	char * const kbdev = "/dev/input/by-id/usb-Dell_Dell_QuietKey_Keyboard-event-kbd";
 	int fd = open(kbdev, O_RDONLY);
@@ -20,6 +29,7 @@ int main ()
 		if (errno) {
 			fprintf(stderr, "main: reason: %s\n", strerror(errno));
 		}
+		tcsetattr(STDIN_FILENO, TCSAFLUSH, terp);
 		exit(EXIT_FAILURE);
 	}
 
@@ -31,12 +41,13 @@ int main ()
 				fprintf(stderr, "main: reason: %s\n", strerror(errno));
 			}
 			close(fd);
+			tcsetattr(STDIN_FILENO, TCSAFLUSH, terp);
 			exit(EXIT_FAILURE);
 		}
 		if (EV_KEY == evp->type) {
-			if (KEY_UP == evp->code) {
+			if (KEY_UP == evp->code && (0 == evp->value)) {
 				fprintf(stdout, "%s\n", "main: key-up");
-			} else if (KEY_DOWN == evp->code) {
+			} else if ((KEY_DOWN == evp->code) && (0 == evp->value)) {
 				fprintf(stdout, "%s\n", "main: key-down");
 			} else if (KEY_ESC == evp->code) {
 				fprintf(stdout, "%s\n", "main: key-esc");
@@ -47,8 +58,16 @@ int main ()
 	}
 
 	close(fd);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, terp);
 	return 0;
 }
 
+// accessing keyboard input:
 // references: https://stackoverflow.com/questions/20943322/accessing-keys-from-linux-input-device
 // https://www.kernel.org/doc/Documentation/input/input.txt
+//
+// disabling terminal input echoing
+// https://www.gnu.org/software/libc/manual/html_node/Low_002dLevel-Terminal-Interface.html
+// https://www.gnu.org/software/libc/manual/html_node/getpass.html
+// also see `man tcsetattr` for info on `TCSAFLUSH`
+// https://stackoverflow.com/questions/59922972/how-to-stop-echo-in-terminal-using-c/59923166#59923166
